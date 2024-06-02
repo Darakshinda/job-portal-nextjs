07c47949d3d6772dbbe7b6eb24a9cbd029e11567
Darakshinda <2021csb1130@iitrpr.ac.in>
2024-06-03 01:32:24 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,17 +38,26 @@ export default function Home() {
     const currentTime = new Date();
     const createdAtTime = new Date(createdAt);
     const timeDifference = currentTime.getTime() - createdAtTime.getTime();
+  
+    if (timeDifference < 5000) { // If less than 10 seconds
+      return "Just now";
+    }
+  
     const minutes = Math.floor(timeDifference / 60000);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
+  
     if (days > 0) {
       return `${days}d`;
     } else if (hours > 0) {
       return `${hours}h`;
-    } else {
+    } else if (minutes > 0) {
       return `${minutes}m`;
+    } else {
+      return `${timeDifference / 1000}s`; // Convert milliseconds to seconds
     }
   };
+  
 
   return (
     <main>
